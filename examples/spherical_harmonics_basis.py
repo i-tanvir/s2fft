7ee8b3c7bf58060ee8eff82476b8e2782a8bb9db
Tanvir <i.tanvir135@gmail.com>
2026-08-20 12:59:10 +0100

    Adds file for spherical harmonics basis & adds intro

---
+++ examples/spherical_harmonics_basis.py
@@ -0,0 +1,10 @@
+"""
+Understanding spherical harmonics
+=================================
+
+Spherical harmonics are the spherical counterpart of Fourier waves.
+This tutorial introduces their main mathematical properties, visualises
+some basis functions, and shows how ''S2FFT'' decomposes a signal into
+spherical harmonic coefficients
+"""
+
